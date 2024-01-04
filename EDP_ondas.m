939d5c5d0de7c64c38e97b58e4e328a60da50647
asiermhpsm <118305795+asiermhpsm@users.noreply.github.com>
2024-01-04 22:27:25 +0100

    ondas hecho

--- EDP_ondas.m
+++ EDP_ondas.m
@@ -1,13 +1,15 @@
-funcion_str = input('Ingrese la función.\nSugerencia:-cos(pi*x/3)+1\nf(x): ', 's');
+funcion_str = input('Ingrese la función.\nSugerencia:cos(4*x)\nf(x): ', 's');
 f = str2func(['@(x) ', funcion_str]);
-
-if f(0)~=0
-    error('NO SE CUMPLEN LA CONDICIONES NECESARIAS. f(0)!=0'); 
-end
 h= 0.00001;
-df_6 = (f(6+h) - f(6))/(h);
-if abs(f(6)- df_6) > h
-    error('NO SE CUMPLEN LA CONDICIONES NECESARIAS. f(6)!=f´(6)'); 
+
+df_0 = (f(0+h) - f(0))/(h);
+if abs(df_0) > 0.01
+    error('NO SE CUMPLEN LA CONDICIONES NECESARIAS. f´(0)!=0'); 
+end
+
+df_pi = (f(pi+h) - f(pi))/(h);
+if abs(df_pi) > 0.01
+    error('NO SE CUMPLEN LA CONDICIONES NECESARIAS. f´(pi)!=0'); 
 end
 
 N = 50;
@@ -48,12 +50,17 @@ while true
 end
 
 function resultado = u(x, t, N, f)
-    suma = 0;
+    intgrand0 = @(z) f(z);
+    c0 = 1/pi*integral(intgrand0, 0, pi);
+    integrand1 = @(z) f(z) .* cos(z);
+    c1 = 2/pi*integral(integrand1, 0, pi);
+    suma = c0 + c1 .* cos(x) .* exp(-t) .* (1 + t);
 
-    for n = 0:N
-        integrand = @(z) f(z) ./ exp(z) .* sin((1+2*n)*pi/12 .*z);
-        cn = 1/3*integral(integrand, 0, 6);
-        termino = cn .* exp(x) .* sin((1+2*n)*pi/12 .*x) .* exp(-(1+((1+2*n)*pi/12)^2).*t);
+    for n = 2:N
+        integrand = @(z) f(z) .* cos(n .*z);
+        cn = 2/pi*integral(integrand, 0, pi);
+        betan = sqrt(n^2 - 1);
+        termino = cn .* cos(n.*x) .* exp(-t) .* (cos(betan .* t) + 1./betan .* sin(betan .* t));
         suma = suma + termino;
     end
 
